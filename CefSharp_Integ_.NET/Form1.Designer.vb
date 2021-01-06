300cf351879319ac4a28a23def088e51abbbb542
DenisArg <22160580+DenisArg@users.noreply.github.com>
2021-01-06 17:22:36 -0300

    Ultima Version
    Ultima Version funcional del Navegador

---
+++ CefSharp_Integ_.NET/Form1.Designer.vb
@@ -0,0 +1,131 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.panBrowser = New System.Windows.Forms.Panel()
+        Me.TabControl1 = New System.Windows.Forms.TabControl()
+        Me.TabPage1 = New System.Windows.Forms.TabPage()
+        Me.cmdEnvioDatos = New System.Windows.Forms.Button()
+        Me.TxT_URL = New System.Windows.Forms.TextBox()
+        Me.cmbSalir = New System.Windows.Forms.Button()
+        Me.txtcuit = New System.Windows.Forms.TextBox()
+        Me.TabControl1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'panBrowser
+        '
+        Me.panBrowser.Anchor = CType((((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
+            Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.panBrowser.Location = New System.Drawing.Point(862, 211)
+        Me.panBrowser.Name = "panBrowser"
+        Me.panBrowser.Size = New System.Drawing.Size(1282, 124)
+        Me.panBrowser.TabIndex = 5
+        Me.panBrowser.Visible = False
+        '
+        'TabControl1
+        '
+        Me.TabControl1.Anchor = CType((((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
+            Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.TabControl1.Controls.Add(Me.TabPage1)
+        Me.TabControl1.Location = New System.Drawing.Point(12, 41)
+        Me.TabControl1.Name = "TabControl1"
+        Me.TabControl1.SelectedIndex = 0
+        Me.TabControl1.Size = New System.Drawing.Size(1318, 529)
+        Me.TabControl1.TabIndex = 4
+        '
+        'TabPage1
+        '
+        Me.TabPage1.Location = New System.Drawing.Point(4, 22)
+        Me.TabPage1.Name = "TabPage1"
+        Me.TabPage1.Padding = New System.Windows.Forms.Padding(3)
+        Me.TabPage1.Size = New System.Drawing.Size(1310, 503)
+        Me.TabPage1.TabIndex = 0
+        Me.TabPage1.Text = "Notarinteg - .Net"
+        Me.TabPage1.UseVisualStyleBackColor = True
+        '
+        'cmdEnvioDatos
+        '
+        Me.cmdEnvioDatos.Location = New System.Drawing.Point(16, 12)
+        Me.cmdEnvioDatos.Name = "cmdEnvioDatos"
+        Me.cmdEnvioDatos.Size = New System.Drawing.Size(488, 23)
+        Me.cmdEnvioDatos.TabIndex = 0
+        Me.cmdEnvioDatos.Text = "Click para Enviar Datos a la Pagina"
+        Me.cmdEnvioDatos.UseVisualStyleBackColor = True
+        '
+        'TxT_URL
+        '
+        Me.TxT_URL.Location = New System.Drawing.Point(845, 15)
+        Me.TxT_URL.Name = "TxT_URL"
+        Me.TxT_URL.Size = New System.Drawing.Size(100, 20)
+        Me.TxT_URL.TabIndex = 12
+        Me.TxT_URL.Visible = False
+        '
+        'cmbSalir
+        '
+        Me.cmbSalir.Anchor = CType((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.cmbSalir.Location = New System.Drawing.Point(1051, 12)
+        Me.cmbSalir.Name = "cmbSalir"
+        Me.cmbSalir.Size = New System.Drawing.Size(275, 23)
+        Me.cmbSalir.TabIndex = 2
+        Me.cmbSalir.Text = "Volver"
+        Me.cmbSalir.UseVisualStyleBackColor = True
+        '
+        'txtcuit
+        '
+        Me.txtcuit.Location = New System.Drawing.Point(493, 12)
+        Me.txtcuit.Name = "txtcuit"
+        Me.txtcuit.Size = New System.Drawing.Size(100, 20)
+        Me.txtcuit.TabIndex = 50
+        Me.txtcuit.Visible = False
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(1342, 582)
+        Me.Controls.Add(Me.txtcuit)
+        Me.Controls.Add(Me.cmbSalir)
+        Me.Controls.Add(Me.TxT_URL)
+        Me.Controls.Add(Me.cmdEnvioDatos)
+        Me.Controls.Add(Me.panBrowser)
+        Me.Controls.Add(Me.TabControl1)
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.Name = "Form1"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Notarinteg - .Net"
+        Me.TabControl1.ResumeLayout(False)
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents panBrowser As Panel
+    Friend WithEvents TabControl1 As TabControl
+    Friend WithEvents TabPage1 As TabPage
+    Friend WithEvents cmdEnvioDatos As Button
+    Friend WithEvents TxT_URL As TextBox
+    Friend WithEvents cmbSalir As Button
+    Friend WithEvents txtcuit As TextBox
+End Class
---
+++ CefSharp_Integ_.NET/Form1.vb
@@ -0,0 +1,173 @@
+﻿Imports CefSharp.WinForms
+Imports CefSharp
+
+Public Class Form1
+    Public Property ScriptErrorsSuppressed As Boolean
+    'Objeto que recoje los datos leidos de la tabla
+    Dim dt As New DataTable
+    'Contador del número de filas
+    Dim intFila As Integer = 0
+    Dim int As Integer = 0
+
+    Private WithEvents browser As ChromiumWebBrowser
+    Public Sub New()
+        InitializeComponent()
+        Dim settings As New CefSettings()
+        CefSharp.Cef.Initialize(settings)
+        '/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+        Dim UrlUsar As String
+        'Objeto conexión para conectarse a la base de datos de Access
+        Dim Snombre_Base As String
+        Dim adaptador As System.Data.OleDb.OleDbDataAdapter
+        Dim strConexion As System.Data.OleDb.OleDbConnection
+        'Variable que contiene la select a ejecutar sobre la base de datos
+        Dim strSQL As String
+        'Adaptador de la base de datos con la aplicación
+        Snombre_Base = "C:\Dominial\dB\IntercambioNET.mdb"
+        strSQL = "SELECT * FROM Datos_NET"
+        strConexion = New System.Data.OleDb.OleDbConnection("provider=Microsoft.Jet.OLEDB.4.0;data source=" & Snombre_Base & "")
+        adaptador = New OleDb.OleDbDataAdapter(strSQL, strConexion)
+        Dim ds As New DataSet
+        'Rellena al adaptador con el primer dato leído
+        adaptador.Fill(dt)
+        'Vacía la memoria del adaptador
+        adaptador.Dispose()
+        'llama a la subrutina qu emuestra el texto en la caja
+        UrlUsar = CType(dt.Rows(intFila)("UrlDestino"), String)
+        '/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+        browser = New ChromiumWebBrowser("" & UrlUsar) With {.Dock = DockStyle.Fill}
+        browser.Name = "Navegador Integ"
+        TabControl1.SelectedTab.Controls.Add(browser)
+        TxT_URL.Text = UrlUsar
+        browser.Dock = DockStyle.Fill
+
+    End Sub
+
+    Private Sub cmdEnvioDatos_Click(sender As Object, e As EventArgs) Handles cmdEnvioDatos.Click
+        'CUIL
+        Dim CUIL_CUIT As String, Nombre As String, Apellido As String, Genero As String, FechaNacimiento As Date, NroPag As Integer
+        'Objeto conexión para conectarse a la base de datos de Access
+        'Dim strConexion As New OleDbConnection("Provider=Microsoft.jet.Oledb.4.0;Data Source=" + My.Application.Info.DirectoryPath + "\Database21.mdb")
+        Dim Snombre_Base As String
+        Dim adaptador As System.Data.OleDb.OleDbDataAdapter
+        Dim strConexion As System.Data.OleDb.OleDbConnection
+        'Variable que contiene la select a ejecutar sobre la base de datos
+        Dim strSQL As String
+        'Adaptador de la base de datos con la aplicación
+        Snombre_Base = "C:\Dominial\dB\IntercambioNET.mdb"
+        strSQL = "SELECT * FROM Datos_NET"
+        strConexion = New System.Data.OleDb.OleDbConnection("provider=Microsoft.Jet.OLEDB.4.0;data source=" & Snombre_Base & "")
+        adaptador = New OleDb.OleDbDataAdapter(strSQL, strConexion)
+        Dim ds As New DataSet
+        'Rellena al adaptador con el primer dato leído
+        adaptador.Fill(dt)
+        'Vacía la memoria del adaptador
+        adaptador.Dispose()
+        NroPag = CType(dt.Rows(intFila)("NroPag"), Integer)
+        If NroPag = 1 Then
+
+            CUIL_CUIT = CType(dt.Rows(intFila)("CUIL_CUIT"), String)
+            'TipoDoc = dt.Rows(intFila)("TipoDoc")
+            Nombre = CType(dt.Rows(intFila)("Nombre"), String)
+            Apellido = CType(dt.Rows(intFila)("Apellido"), String)
+            Genero = CType(dt.Rows(intFila)("Genero"), String)
+            FechaNacimiento = CType(dt.Rows(intFila)("FechaNacimiento"), Date)
+
+            With browser
+                'Complete web form
+                Dim identifiers As List(Of Long) = .GetBrowser.GetFrameIdentifiers()
+                For Each i As Long In identifiers
+                    Try
+                        Dim frame As CefSharp.IFrame = .GetBrowser.GetFrame(i)
+                        frame.EvaluateScriptAsync("document.getElementById('edit-lastname').value='" & Apellido & "';")
+                        'frame.EvaluateScriptAsync("document.getElementById('ctl00_Contenido_Apellido_Casada').value=" & Val(CUIL_CUIT) & ";")
+                        frame.EvaluateScriptAsync("document.getElementById('edit-name').value='" & Nombre & "';")
+                        If Genero = "FEMENINO" Then
+                            frame.EvaluateScriptAsync("document.getElementById('edit-sexo-f').click();")
+                        End If
+                        If Genero = "MASCULINO" Then
+                            frame.EvaluateScriptAsync("document.getElementById('edit-sexo-m').click();")
+                        End If
+                        frame.EvaluateScriptAsync("document.getElementById('edit-nro-doc').value=" & Val(CUIL_CUIT) & ";")
+                        frame.EvaluateScriptAsync("document.getElementById('edit-date').value='" & FechaNacimiento.ToString(“dd/MM/yyyy”) & "';")
+                        'MsgBox(CUIL_CUIT)
+                        'frame.EvaluateScriptAsync("document.getElementById('ctl00_Contenido_Estado_Civil').value='26813871';")
+                        frame.EvaluateScriptAsync("document.getElementById('edit-security-code').click();")
+                        'frame.EvaluateScriptAsync("document.getElementById('paw').value='1234';")
+                        'frame.EvaluateScriptAsync("document.getElementById('btgo').click();")
+                        Exit For
+                    Catch ex As Exception
+                    End Try
+                Next
+            End With
+        End If
+
+        If NroPag = 2 Then
+            CUIL_CUIT = CType(dt.Rows(intFila)("CUIL_CUIT"), String)
+            txtcuit.Visible = False
+            txtcuit.Text = CType(dt.Rows(intFila)("CUIL_CUIT"), String)
+            txtcuit.SelectAll()
+            txtcuit.Copy()
+            My.Computer.Keyboard.SendKeys("{TAB}", True)
+            My.Computer.Keyboard.SendKeys("{TAB}", True)
+            My.Computer.Keyboard.SendKeys("{TAB}", True)
+            'My.Computer.Keyboard.SendKeys("{TAB}", True)
+            My.Computer.Keyboard.SendKeys("(^(v))", True)
+            My.Computer.Keyboard.SendKeys("{TAB}", True)
+            'With browser
+            'Complete web form
+            'Dim identifiers As List(Of Long) = .GetBrowser.GetFrameIdentifiers()
+            'For Each i As Long In identifiers
+            'Try
+            'Dim frame As CefSharp.IFrame = .GetBrowser.GetFrame(i)
+            'frame.EvaluateScriptAsync("document.getElementById('cuit').value='" & Val(CUIL_CUIT) & "';")
+            'frame.EvaluateScriptAsync("document.getElementById('cuit').value='78877';")
+            'frame.ExecuteJavaScriptAsync("document.getElementById('cuit').value='20022';")
+            'frame.EvaluateScriptAsync("document.getElementByname('cuit').value='78877';")
+            ''MsgBox(CUIL_CUIT)
+            ''frame.EvaluateScriptAsync("document.getElementById('').click();")
+            'Exit For
+            'Catch ex As Exception
+            'End Try
+            'Next
+            'End With
+        End If
+    End Sub
+
+    Private Sub cmbSalir_Click(sender As Object, e As EventArgs) Handles cmbSalir.Click
+        Me.Close()
+
+    End Sub
+    Public Class DownloadHandler2
+        'Inherits IDownloadHandler
+
+        Public Sub OnBeforeDownload(ByVal browser As IBrowser, ByVal downloadItem As DownloadItem, ByVal callback As IBeforeDownloadCallback)
+            downloadItem.SuggestedFileName = "c:\temp\temp.pdf"
+            MsgBox("Public Sub OnBeforeDownload(ByVal browser As IBrowser, ByVal downloadItem As DownloadItem, ByVal callback As IBeforeDownloadCallback)")
+            If Not callback.IsDisposed Then
+                MsgBox("If Not callback.IsDisposed Then")
+                Using callback
+                    callback.[Continue](downloadItem.SuggestedFileName, showDialog:=False)
+                End Using
+            End If
+        End Sub
+
+        Public Sub OnDownloadUpdated(ByVal browser As IBrowser, ByVal downloadItem As DownloadItem, ByVal callback As IDownloadItemCallback)
+            If Not downloadItem.IsInProgress AndAlso browser.IsPopup AndAlso Not browser.HasDocument Then
+                browser.GetHost().CloseBrowser(False)
+            End If
+        End Sub
+    End Class
+    Public Function OnBeforePopup(ByVal browser As IWebBrowser, ByVal sourceUrl As String, ByVal targetUrl As String, ByRef x As Integer, ByRef y As Integer, ByRef width As Integer, ByRef height As Integer) As Boolean
+        '    Dim brf As Object = Nothing
+        '    brf.InvokeOnUiThreadIfRequired(
+        '        Function()
+        '    Dim bf = New BRForm(targetUrl) With {.Visible = True}
+        ' End Function)
+        '    Return True
+    End Function
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+End Class
